d30806e7d233f87d38f678bf50c79cb3b7811332
Ryan Harvey <Ryan.Harvey@arm.com>
2024-06-07 16:22:00 +0100

    I just want linting

--- lua/plugins/nonelsp.lua
+++ lua/plugins/nonelsp.lua
@@ -9,8 +9,7 @@ return {
   init = function()
     local null_ls = require("null-ls")
     local sources = {
-        require("none-ls-shellcheck.diagnostics"),
-        require("none-ls-shellcheck.code_actions"),
+        require("none-ls-shellcheck.diagnostics")
     }
     null_ls.setup({sources = sources})
   end
